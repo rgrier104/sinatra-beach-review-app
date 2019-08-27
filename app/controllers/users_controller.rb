72f1c471836b467a91baf99287f8555efad80d23
Rachel Grier <rgrier104@gmail.com>
2019-08-27 02:11:27 +0000

    add user and password authentication and redirect to index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,19 @@ class UsersController < ApplicationController
     erb :'/login'
   end
 
+  #receive the login form, find the user, and log the user in
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    #authenticate password and
+    if @user.authenticate(params[:password]) && user
+      session[:user_id] = user.id
+      redirect to "/user/index"
+    else
+      redirect to "/login"
+    end
+  end
+
   get '/logout' do
     erb :'/user/logout'
   end

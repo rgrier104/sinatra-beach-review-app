5ea0ef4227924b91c7b7012c0c4cffc1dd94b910
Rachel Grier <rgrier104@gmail.com>
2019-08-29 21:00:06 +0000

    add post route for signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
 
-  #the purpose of this route is to render  the signup page
+  #the purpose of this route is to render the signup page
   get '/signup' do
     erb :'/signup'
   end
 
+  #receive the signup form, create the user, and log the user in
+  post '/signup' do
+    @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    session[:user_id] = @user.id
+
+    redirect "/users/#{:@user.username}"
+  end
+
+  #the purpose of this route is to render the login page
   get '/login' do
     erb :'/login'
   end
@@ -13,16 +22,18 @@ class UsersController < ApplicationController
   post '/login' do
     @user = User.find_by(username: params[:username])
 
-    #authenticate password and
+    #authenticate password and confirm user has account in database
     if @user.authenticate(params[:password]) && user
       session[:user_id] = user.id
-      redirect to "/users/#{:@user.id}"
+      redirect to "/users/#{:@user.username}"
     else
       redirect to "/login"
     end
   end
 
-  get '/users/:id' do
+  get '/users/:username' do
+    @user = User.find(session[:user_id])
+
     erb :'user/index'
   end
 

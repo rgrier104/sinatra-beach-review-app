7d42c29f0c961e1fcbf5f710e7e906fd5cff4c29
Rachel Grier <rgrier104@gmail.com>
2019-09-04 23:21:46 +0000

    add methods to Beach class tocreate slug and find by slug name

--- app/models/beach.rb
+++ app/models/beach.rb
@@ -1,4 +1,16 @@
 class Beach < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  def slug
+    name = self.name.downcase.gsub(' ','-')
+  end
+
+  def self.find_by_slug(slug)
+    Beach.all.find do |beach|
+      beach.slug == slug
+    end
+  end
+
+
 end

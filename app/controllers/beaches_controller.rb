8ce4b2fd0935f9a8811bb7c0017386cad52279ef
Rachel Grier <rgrier104@gmail.com>
2019-09-05 00:13:56 +0000

    add route for beaches show page

--- app/controllers/beaches_controller.rb
+++ app/controllers/beaches_controller.rb
@@ -1,8 +1,16 @@
 class BeachesController < ApplicationController
 
+  #render beaches index page
   get '/beaches' do
-    
+
     erb :'/beaches/index'
+  end
+
+  #render show page that lists beach reviews for specified beach
+  get '/beaches/:slug' do
+    @beach = Beach.find_by_slug(params[:slug])
+
+    erb :'/beaches/show'
   end
 
 
